cd47c87c7b7a79bab28b47eee91980dd09affebb
Bárbara Araújo Paim <barbaraaraujopaim99@gmail.com>
2024-02-16 13:24:25 -0300

    Initial Commit

---
+++ DocumentBasedApp/DocumentBasedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentBasedAppApp.swift
+//  DocumentBasedApp
+//
+//  Created by Barbara Araujo Paim on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DocumentBasedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
